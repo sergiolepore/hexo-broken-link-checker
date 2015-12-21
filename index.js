c1dbb2274067adffbdf765d641a97184c5ef9dfe
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-21 09:00:23 +0100

    Remove callback

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var commandOptions = require('./lib/commandOptions');
+var Hexo = require('hexo');
+var hexo = new Hexo(process.cwd(), {});
 
 // register command handler
 hexo.extend.console.register('link_checker', commandOptions.desc, commandOptions, require('./lib/command'));
